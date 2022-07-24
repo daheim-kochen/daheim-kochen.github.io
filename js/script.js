2ba131afff2cdd5370daacdce9a3996a9a7921ae
n1v0lg <n1v0lg@users.noreply.github.com>
2022-07-24 17:53:12 +0200

    [WIP] consent nonce check and session id passthrough

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ const option1Selector = "option1"
 const option2Selector = "option2"
 
 const choiceScenarioQueryParam = 'choiceScenario';
+const consentNonceQueryParam = 'consentNonce';
+const consentSessionIdQueryParam = 'consentSessionId';
 
 const locale = 'de';
 const translations = {
@@ -60,6 +62,8 @@ const Type = {
     Veggie: 'Veggie',
     Meat: 'Meat'
 }
+
+const consentNonce = "e4c2790346bf4cbca22b961a324094ae";
 
 const itemSelection = (function () {
     let selectedItem
@@ -144,15 +148,24 @@ $('.open-modal').click(function (_) {
     displaySelected()
 })
 
-function toQualtrixUrl(confirmedType, choiceScenario) {
-    return getTargetUrl() + toQualtrixParam(confirmedType) + "&" + "ChoiceScenario=" + choiceScenario;
+function toQualtrixUrl(confirmedType, choiceScenario, consentSessionId) {
+    return getTargetUrl()
+        + toQualtrixParam(confirmedType)
+        + "&ChoiceScenario=" + choiceScenario
+        + "&ConsentSessionId=" + consentSessionId;
 }
 
 $('.checkout').click(function (event) {
     event.preventDefault()
     const confirmedType = getConfirmedType(itemSelection.confirmedItem().id, itemSelection.choiceScenarioProps())
     const choiceScenario = getParameterByName(choiceScenarioQueryParam)
-    const win = window.open(toQualtrixUrl(confirmedType, choiceScenario), '_self')
+    const consentSessionId = getParameterByName(consentSessionIdQueryParam)
+    const consentNonce = getParameterByName(consentNonceQueryParam)
+    if (consentNonce !== consentNonce) {
+        window.alert("Consent nonce required to submit choice.")
+        return;
+    }
+    const win = window.open(toQualtrixUrl(confirmedType, choiceScenario, consentSessionId), '_self')
     win.focus()
 })
 
@@ -347,6 +360,12 @@ const setPropsForChoiceScenario = (choiceScenario) => {
     }
 }
 
+const withConsentData = (props) => {
+    props["consentNonce"] = getParameterByName(consentNonceQueryParam)
+    props["consentSessionId"] = getParameterByName(consentSessionIdQueryParam)
+    return props
+}
+
 const getParameterByName = (name, url = window.location.href) => {
     name = name.replace(/[\[\]]/g, '\\$&')
     const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
@@ -368,7 +387,7 @@ const localizeElement = element => {
 };
 
 choiceScenario = getParameterByName(choiceScenarioQueryParam)
-const props = setPropsForChoiceScenario(choiceScenario)
+const props = withConsentData(setPropsForChoiceScenario(choiceScenario))
 itemSelection.storeChoiceScenarioProps(props)
 document.addEventListener("DOMContentLoaded", () => {
     // TODO empty/unknown choice scenario
